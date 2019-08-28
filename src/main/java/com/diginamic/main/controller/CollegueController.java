fb3ed59a77603072d4ba7800313d1083eee2e539
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-08-28 20:27:55 +0100

    correction recherche par matricule

--- src/main/java/com/diginamic/main/controller/CollegueController.java
+++ src/main/java/com/diginamic/main/controller/CollegueController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,7 +39,7 @@ public class CollegueController {
 	}
 	
 	@GetMapping(value = "/{matricule}")
-	public ResponseEntity<Collegue> getCollegueByMatricule(String matricule) {
+	public ResponseEntity<Collegue> getCollegueByMatricule(@PathVariable String matricule) {
 		try {
 			Collegue resultat = service.rechercherParMatricule(matricule);
 			return new ResponseEntity<Collegue>(resultat, HttpStatus.OK);
